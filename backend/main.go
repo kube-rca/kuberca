b1b61d927bfbcb452d720bfbb722a4836b4245a2
hjk1996 <dunhill741@naver.com>
2026-01-02 13:33:05 +0900

    refactor: 불필요 공백 제거

--- backend/main.go
+++ backend/main.go
@@ -21,7 +21,6 @@ func main() {
 
 	log.Printf("Embedding API Key: %s", cfg.Embedding.APIKey)
 
-	
 	// 1. DB 연결 풀 초기화
 	ctx := context.Background()
 	dbPool, err := db.NewPostgresPool(ctx, cfg.Postgres)
